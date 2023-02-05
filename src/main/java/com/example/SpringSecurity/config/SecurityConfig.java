5cec741758102b56ac27cc701cc7550423cd6b7b
Mykhailo Vasylenko <mykhailo.development@gmail.com>
2023-02-05 20:22:47 +0200

    Tell spring to use custom UserDetailsService through AuthenticationProvider implementation

--- src/main/java/com/example/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/SpringSecurity/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.example.SpringSecurity.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,6 +12,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -22,6 +28,7 @@ import java.util.Objects;
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
+
 
     private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
             new User(
@@ -37,20 +44,20 @@ public class SecurityConfig {
     );
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
+                .authenticationProvider(authenticationProvider())
                 .addFilterAfter(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
 
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         return new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -61,5 +68,20 @@ public class SecurityConfig {
                         .orElseThrow(() -> new UsernameNotFoundException("No user was found"));
             }
         };
+    }
+
+
+    @Bean
+    private AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 }
